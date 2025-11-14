8bb8b55922f64a0f22165d1435fa335b27bd507b
Joel Mba Bengono <joelmituymbabengono0100@gmail.com>
2025-11-14 19:40:09 +0000

    Created another scene and made code for a scene transition(I think)

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField] private string Level2;  //Name of the scene to load when player enters trigger
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Check if the colliding object is the player
+        if (collision.CompareTag("Player"))
+        {
+            //Load the specified scene when player enters the trigger
+            SceneManager.LoadScene(Level2);
+        }
+    }
+}
