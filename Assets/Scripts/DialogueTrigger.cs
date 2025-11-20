5a89d6cfaaa5ad946fd36dea76d7ee87eede7101
Joel Mba Bengono <joelmituymbabengono0100@gmail.com>
2025-11-20 19:39:08 +0000

    Fixed the dialogue trigger from scene 1

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,12 +4,13 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogueScript;
     private bool playerDetected;
+    private bool hasTriggered = false; // Add this flag to track if already triggered
 
     //Detect trigger with player
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //If we triggered the player enable playerdetected and show indicator
-        if (collision.tag == "Player")
+        //If we triggered the player AND haven't triggered before, enable playerdetected and show indicator
+        if (collision.tag == "Player" && !hasTriggered)
         {
             playerDetected = true;
             dialogueScript.ToggleIndicator(true);
@@ -30,9 +31,12 @@ public class DialogueTrigger : MonoBehaviour
     //While detected if we interact start the dialogue
     private void Update()
     {
-        if (playerDetected && Input.GetKeyDown(KeyCode.F))
+        if (playerDetected && Input.GetKeyDown(KeyCode.F) && !hasTriggered)
         {
             dialogueScript.StartDialogue();
+            hasTriggered = true; // Mark as triggered so it can't happen again
+            playerDetected = false; // Immediately disable detection
+            dialogueScript.ToggleIndicator(false); // Hide the indicator immediately
         }
     }
 }
